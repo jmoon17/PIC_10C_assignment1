a0314f8d3932fe5dc56af17e830733008e114db9
Johan Moon <Moon@wifi-131-179-41-191.host.ucla.edu>
2018-06-10 01:13:19 -0700

    first commit

---
+++ card_game/card_game/siete_y_medio.cpp
@@ -0,0 +1,30 @@
+//
+//  siete_y_medio.cpp
+//  card_game
+//
+//  Created by Johan Moon on 6/10/18.
+//  Copyright © 2018 Johan Moon. All rights reserved.
+//
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "cards.h"
+using namespace std;
+
+// Global constants (if any)
+
+
+// Non member functions declarations (if any)
+
+
+// Non member functions implementations (if any)
+
+
+// Stub for main
+int main(){
+    /* --STATEMENTS-- */
+    return 0;
+}
