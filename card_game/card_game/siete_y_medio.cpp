caf0da7ac257f8f996f7a13859d4434e9efa4f73
Johan Moon <Moon@Johans-MacBook-Pro.local>
2018-06-19 00:29:48 -0700

    commented out readResponse fn, use alternative wayto get response

--- card_game/card_game/siete_y_medio.cpp
+++ card_game/card_game/siete_y_medio.cpp
@@ -22,32 +22,32 @@ using namespace std;
 bool readResponse();
 
 // Non member functions implementations (if any)
-bool readResponse() {
-    bool isYes = true;
-    string answer;
-    // Make a variable to help figure out if the input from user is good
-    bool isGoodInput = false; // assume NOT good to start
-    while (!isGoodInput) {
-        cout << "Do you want another card (y/n)? ";
-        cin >> answer;
-        
-        if (answer == "y" || answer == "Y") {
-            isYes = true;
-            isGoodInput = true;
-        }
-        
-        else if (answer == "n" || answer == "N") {
-            isYes = false;
-            isGoodInput = true;
-        }
-        
-        else {
-            // Ask again because the input is wrong
-            isGoodInput = false;
-        }
-    }
-    return isYes;
-}
+//bool readResponse() {
+//    bool isYes = true;
+//    string answer;
+//    // Make a variable to help figure out if the input from user is good
+//    bool isGoodInput = false; // assume NOT good to start
+//    while (!isGoodInput) {
+//        cout << "Do you want another card (y/n)? ";
+//        cin >> answer;
+//
+//        if (answer == "y" || answer == "Y") {
+//            isYes = true;
+//            isGoodInput = true;
+//        }
+//
+//        else if (answer == "n" || answer == "N") {
+//            isYes = false;
+//            isGoodInput = true;
+//        }
+//
+//        else {
+//            // Ask again because the input is wrong
+//            isGoodInput = false;
+//        }
+//    }
+//    return isYes;
+//}
 
 // Stub for main
 int main()
@@ -57,6 +57,7 @@ int main()
     Player p(100);
     int bet;
     bool yesOrno = true;
+    string response;
     
     while(p.get_money()>0)
     {
@@ -66,48 +67,52 @@ int main()
         cout << ". Enter bet: ";
         cin >> bet;
         }while(bet > p.get_money());
-        
-    //initiate game
-    Hand h;
-
-    Card c;
+     
     
-    //add card to the hand and compute the total value of the card
-    h.add_card(c);
-    h.compute_total(c.get_rank());
-    do
-    {
-    cout << "your cards: " << "\n" << "\t";
-    h.print_hand();
-    
+        //initiate game
+        Hand h;
+        Card c;
+            
+        //add card to the hand and compute the total value of the card
+        h.add_card(c);
+        h.compute_total(c.get_rank());
+            
+        cout << "your cards: " << "\n" << "\t";
+        h.print_hand();
+            
         cout << "Your total is " << h.get_sum() <<". ";
         
-    if(h.get_sum()>7.5)
-    {
-        cout << "You busted!" << endl;
-        break;
-    }
-    readResponse();
-
-    }while(readResponse() == yesOrno);
-    
-    if(readResponse() == yesOrno)
-    {
-        Card c2;
-        h.add_card(c2);
-        c2.draw_card();
+        do{
+            cout << "Do you want another card (y/n)? ";
+            cin >> response;
+        }   while ((response != "y") && (response != "n"));
         
-        cout << "your cards: ";
-        h.print_hand();
-        h.compute_total(c2.get_rank());
-        cout << "Your total is " << h.get_sum();
+        while (response == "y")
+        {
+            Card c2;
+            h.add_card(c2);
+            
+            cout << "New card: " << endl;
+            c2.draw_card();
+            
+            cout << "Your cards: " << endl;
+            h.print_hand();
+            
+            h.compute_total(c2.get_rank());
+            cout << "Your total is " << h.get_sum() << endl;
+            do{
+                cout << "Do you want another card (y/n)? ";
+                cin >> response;
+            }   while ((response != "y") && (response != "n"));
+        }
+
+
     }
-    
-    }
-    
     cout << "You have $0. GAME OVER!" <<"\n";
     cout << "Comeback when you have more money." << "\n";
     cout << "Bye!" <<endl;
     
     return 0;
 }
+
+
